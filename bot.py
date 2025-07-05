c9b7d390ea54ccc68daf8280bcdb986b60ddfd8f
Orkhan <go24kz@gmail.com>
2025-07-05 22:23:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from fastapi import FastAPI, Request
 from aiogram.types import Update
 from config import API_TOKEN, WEBHOOK_URL
 from scheduler import scheduler, send_reminder
-from config import bot, dp
+import asyncio
 
-# Создаём экземпляры
+# Создаём бота и диспетчер
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Установить текущие экземпляры
+# Устанавливаем текущие экземпляры
 bot.set_current(bot)
 dp.set_current(dp)
 
@@ -31,11 +31,9 @@ async def done_handler(message: types.Message):
 async def webhook(request: Request):
     data = await request.json()
     update = Update(**data)
-    print("Update received:", update)
     await dp.process_update(update)
     return {"ok": True}
 
-# Запуск
 @app.on_event("startup")
 async def on_startup():
     await bot.set_webhook(WEBHOOK_URL)
